f9b2fe5ec00780660c11903cb8e01898b50e76fc
Aqeel Ghani <aqeel70106@gmail.com>
2026-04-13 09:00:49 +0700

    Kembali tanpa variasu pointer

--- soal1.c
+++ soal1.c
@@ -11,20 +11,20 @@
  #include <stdio.h>
   
  int main() {
-    int *izin, *suhu, *jam;
-    unsigned int *radiasi;
+    int izin, suhu, jam;
+    unsigned int radiasi;
 
     // Input Parameter Peneliti
-    scanf("%d %d %d %d", izin, suhu, radiasi, jam);
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
 
    //  Conditional (Sesuai Modul)
-   if(*radiasi >= 6) printf("TOLAK\n");
-   else if (*suhu >= 390) printf("KARANTINA\n");
-   else if (*izin == 1 && *jam < 6 || *jam > 20) printf("TOLAK\n");
-   else if (*izin == 1) printf("MASUK\n");
-   else if (*izin == 2 && *radiasi <= 2 && (*jam >= 8 && *jam <= 18)) printf("MASUK\n");
-   else if (*izin == 2) printf("PEMERIKSAAN\n");
-   else if (*izin == 3 && *radiasi == 0 && *suhu < 380) printf("MASUK\n");
+   if(radiasi >= 6) printf("TOLAK\n");
+   else if (suhu >= 390) printf("KARANTINA\n");
+   else if (izin == 1 && jam < 6 || jam > 20) printf("TOLAK\n");
+   else if (izin == 1) printf("MASUK\n");
+   else if (izin == 2 && radiasi <= 2 && (jam >= 8 && jam <= 18)) printf("MASUK\n");
+   else if (izin == 2) printf("PEMERIKSAAN\n");
+   else if (izin == 3 && radiasi == 0 && suhu < 380) printf("MASUK\n");
    else printf("TOLAK\n");
 
    return 0;
